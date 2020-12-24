569ba1f9c1decae84e53f15dbbac21a21c8c2afe
siddharthPriyadarshi <74820758+siddharthPriyadarshi@users.noreply.github.com>
2020-12-24 12:46:34 +0530

    linked: css and js

---
+++ main.js
@@ -0,0 +1,9 @@
+var txtarea = document.querySelector("#txt-area");
+var btntranslate = document.querySelector("#button-translate");
+
+function clickHandler() {
+    console.log("clicked");
+}
+
+btntranslate.addEventListener("click",clickHandler);
+
