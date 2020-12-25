6769acb8eab20f8de0861d12b5a336ddc2e8b614
siddharthPriyadarshi <74820758+siddharthPriyadarshi@users.noreply.github.com>
2020-12-25 23:12:32 +0530

    wiring js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-var txtarea = document.querySelector("#txt-area");
+var input = document.querySelector("#txtarea-input");
 var btntranslate = document.querySelector("#button-translate");
+var output = document.querySelector("#txtarea-output");
 
 function clickHandler() {
-    console.log("clicked");
+    console.log(input.value);
+    console.log(output);
 }
 
 btntranslate.addEventListener("click",clickHandler);
