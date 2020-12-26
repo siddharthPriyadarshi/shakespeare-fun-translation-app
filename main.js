79f4a0332fd3c7cd085c37b6fb848eac151bbea3
siddharthPriyadarshi <74820758+siddharthPriyadarshi@users.noreply.github.com>
2020-12-26 12:38:16 +0530

    done

--- main.js
+++ main.js
@@ -2,10 +2,26 @@ var input = document.querySelector("#txtarea-input");
 var btntranslate = document.querySelector("#button-translate");
 var output = document.querySelector("#txtarea-output");
 
-function clickHandler() {
-    console.log(input.value);
-    console.log(output);
+
+var serverurl = "https://api.funtranslations.com/translate/shakespeare.json";
+
+function getGeneratedUrl() {
+    var url = serverurl + "?text=" + input.value;
+    return url;
 }
+
+function errorHandler(error) {
+    console.log("error occured",error);
+    alert("Something is wrong! \nPlease try again after a moment");
+}
+
+function clickHandler() {
+    fetch(getGeneratedUrl())
+    .then(response => response.json())
+    .then(json => output.innerText=json.contents.translated)
+    .catch(errorHandler)
+}
+// output.innerText=json.contents.translated
 
 btntranslate.addEventListener("click",clickHandler);
 
